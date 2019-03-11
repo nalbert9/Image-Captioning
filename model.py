52fef1f4a5c8bb2e7363e8d97d447631cbdc8664
gfoxx29 <nalbert9@hotmail.fr>
2019-03-11 07:29:35 +0100

    Defining CNN-RNN Architecture

--- model.py
+++ model.py
@@ -23,10 +23,31 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        
+        super(DecoderRNN, self).__init__()
+        
+        self.hidden_size = hidden_size
+        
+        #self.embedding = nn.Embedding(output_size, hidden_size)
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        
+        # Define LSTM
+        # The LSTM takes Embedded image as inputs, and outputs hidden states
+        # nn.LSTM(input_size, hidden_size, num_layers)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers)
+        
+        #self.out = nn.Linear(hidden_size, output_size)
+        self.out = nn.Linear(hidden_size, vocab_size)
+        
     
     def forward(self, features, captions):
-        pass
+        
+        captions = captions[:,:-1]       
+        embeddings = self.embed(captions)              
+        inputs = torch.cat((features.unsqueeze(1), embeddings), 1)
+        hiddens, _ = self.lstm(inputs)
+        outputs = self.out(hiddens)
+        return outputs
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
