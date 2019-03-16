c610e9a0506b1f62c37dcbcca82c7d8d6003bbd7
gfoxx29 <nalbert9@hotmail.fr>
2019-03-16 20:16:19 +0100

    Define CNNEncoder-RNNDecoder Architecture

--- model.py
+++ model.py
@@ -35,12 +35,16 @@ class DecoderRNN(nn.Module):
         
         super(DecoderRNN, self).__init__()
         
+        self.hidden_size = hidden_size
+        
         # embedding layer that turns words into a vector of a specified size
         # self.embedding = nn.Embedding(output_size, hidden_size)
-        self.embed_img = nn.Embedding(vocab_size, embed_size)
+        self.word_embeddings = nn.Embedding(vocab_size, embed_size)
         
         # The LSTM takes Embedded image as inputs
         # and outputs hidden states of hidden_size
+        # self.lstm = nn.LSTM(input_size, n_hidden, n_layers, 
+        #                    dropout=drop_prob, batch_first=True)
         self.lstm = nn.LSTM(embed_size, hidden_size, num_layers)
         
         # the linear layer that maps the hidden state output dimension 
@@ -54,28 +58,30 @@ class DecoderRNN(nn.Module):
         - captions : Tensor corresponding to the last batch of captions .
         
         """
-        captions = captions[:,:-1]    
+        captions = captions[:,:-1] # Reshaping from "torch.Size([10, 17])" to "torch.Size([10, 16])"
         
         # Initialize the hidden state
-        self.hidden = self.initHidden() 
+        batch_size = features.shape[0] # Notebook 1
+        self.hidden = self.init_hidden(batch_size) 
         
-        embeds = self.embed_img(captions)  
+        embeds = self.word_embeddings(captions)  #words
         
+        # Concatenating features to embedding
         # torch.cat 3D tensors
         # imputs = torch.cat((input, hidden), 1)
-        inputs = torch.cat((features.unsqueeze(1), embeddings), 1)
+        inputs = torch.cat((features.unsqueeze(1), embeds), 1)
+        
         hiddens, _ = self.lstm(inputs)
-
         outputs = self.out(hiddens)
         return outputs
     
-    def initHidden(self):
+    def init_hidden(self,batch_size):
         """Returns the initial decoder state,
         conditioned on the final encoder state."""
         
         # The axes dimensions are (n_layers, batch_size, hidden_size)
-        return (torch.zeros(1, 1, self.hidden_size),
-                torch.zeros(1, 1, self.hidden_size))
+        return (torch.zeros(1, batch_size, self.hidden_size),
+                torch.zeros(1, batch_size, self.hidden_size))
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
