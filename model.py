3192d655f008371c212694ae7004f87038383c6c
gfoxx29 <nalbert9@hotmail.fr>
2019-04-25 06:02:03 +0200

    Initialize weights

--- model.py
+++ model.py
@@ -5,6 +5,8 @@ import torchvision.models as models
 
 class EncoderCNN(nn.Module):
     def __init__(self, embed_size):
+    	"""Load the pretrained ResNet50 and replace top fc layer."""
+
         super(EncoderCNN, self).__init__()
         resnet = models.resnet50(pretrained=True)
         for param in resnet.parameters():
@@ -13,17 +15,26 @@ class EncoderCNN(nn.Module):
         modules = list(resnet.children())[:-1]
         self.resnet = nn.Sequential(*modules)
         self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+        self.init_weights()
 
+    
     def forward(self, images):
+    	"""Extract the image feature vectors."""
+
         features = self.resnet(images)
         features = features.view(features.size(0), -1)
         features = self.embed(features)
         return features
-    
+
+    def init_weights(self):
+    	"""Initialize the weights."""
+    	self.self.linear.weight.data.normal_(0.0, 0.02)
+    	self.linear.bias.data.fill_(0)
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
         """
+        Set the hyper-parameters and build the layers.
         Parameters
         ----------
         - embed_size  : Dimensionality of image and word embeddings
@@ -32,9 +43,7 @@ class DecoderRNN(nn.Module):
         - num_layers  : Number of layers
         
         """
-        
         super(DecoderRNN, self).__init__()
-        
         self.hidden_size = hidden_size
         
         # embedding layer that turns words into a vector of a specified size
@@ -49,16 +58,13 @@ class DecoderRNN(nn.Module):
         
         # the linear layer that maps the hidden state output dimension 
         self.linear = nn.Linear(hidden_size, vocab_size)
+        self.init_weights()
         
     
     def forward(self, features, captions):
+    	"""Extract the image feature vectors."""
 
         captions = captions[:,:-1] 
-        
-        # Initialize the hidden state
-        # batch_size = features.shape[0]
-        # self.hidden = self.init_hidden(batch_size) 
-        
         embeds = self.word_embeddings(captions)
         
         # Concatenating features to embedding
@@ -70,21 +76,18 @@ class DecoderRNN(nn.Module):
         
         return outputs
     
-    def init_hidden(self,batch_size):
-        """
-        Returns the initial decoder state,
-        conditioned on the final encoder state.
-        """
-        
-        # The axes dimensions are (n_layers, batch_size, hidden_size)
-        return (torch.zeros(1, batch_size, self.hidden_size),
-                torch.zeros(1, batch_size, self.hidden_size))
+    def init_weights(self):
+        """Initialize weights."""
 
+        self.embed.weight.data.uniform_(-0.1, 0.1)
+        self.linear.weight.data.uniform_(-0.1, 0.1)
+        self.linear.bias.data.fill_(0)
+        
     def sample(self, inputs, states=None, max_len=20):
         """
-        accepts pre-processed image tensor (inputs) and returns predicted 
-        sentence (list of tensor ids of length max_len)
-        
+		Greedy search:
+        Samples captions for pre-processed image tensor (inputs) 
+        and returns predicted sentence (list of tensor ids of length max_len)
         """
         
         predicted_sentence = []
@@ -93,6 +96,7 @@ class DecoderRNN(nn.Module):
             
             lstm_out, states = self.lstm(inputs, states)
 
+            lstm_out = lstm_out.squeeze(1)
             lstm_out = lstm_out.squeeze(1)
             outputs = self.linear(lstm_out)
             
