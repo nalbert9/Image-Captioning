9778742bac39103aa2a3507081f2b735f73ddd7c
gfoxx29 <nalbert9@hotmail.fr>
2019-04-29 12:18:00 +0200

    Change pretrained model to Resnet-152

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ class EncoderCNN(nn.Module):
     	"""Load the pretrained ResNet50 and replace top fc layer."""
 
         super(EncoderCNN, self).__init__()
-        resnet = models.resnet50(pretrained=True)
+        resnet = models.resnet152(pretrained=True)
         for param in resnet.parameters():
             param.requires_grad_(False)
         
